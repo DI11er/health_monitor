ec3d86ca5d1eed5623164d009811f4cf10949805
Diller <makarenko.stepan@bk.ru>
2023-07-16 14:18:47 +0300

    Добавил настройку для способов логирования в файл\консоль

--- docker/web/config.py
+++ docker/web/config.py
@@ -17,12 +17,33 @@ class Status(Enum):
     stopped = 'grey'
     un_status = None
 
+# DIRS
 CURRENT_DIR = Path.cwd() / env.str('WORK_DIR', default='.')
 
 RESOURCES_DIR = CURRENT_DIR / 'resources'
 
-logging.basicConfig(filename=RESOURCES_DIR / 'logs' / 'sample.log', level=logging.DEBUG)
-#logging.basicConfig(level=logging.INFO)
+LOG_DIR = RESOURCES_DIR / 'logs'
+
+# Logs config
+LOG_MODE = env.str("LOG_MODE", 'dev')
+
+if LOG_MODE == "prod":
+    # logging in prod mode
+    if not Path.exists(LOG_DIR):
+        Path.mkdir(LOG_DIR)
+    logging.basicConfig(
+        format=u'%(threadName)s\t%(filename)s\t[LINE:%(lineno)d]# %(levelname)-8s\t [%(asctime)s]  %(message)s',
+        level="INFO",
+        handlers=[logging.FileHandler(LOG_DIR / "log.log", 'w', 'utf-8')]
+    )
+elif LOG_MODE == "dev":
+    # logging in dev mode
+    logging.basicConfig(
+        format=u'%(threadName)s\t%(filename)s\t[LINE:%(lineno)d]# %(levelname)-8s\t [%(asctime)s]  %(message)s',
+        level="DEBUG",
+        handlers=[logging.StreamHandler()]
+    )
+
 
 DEBUG = env.bool('DEBUG', default=True)
 
@@ -36,12 +57,10 @@ DISCOVERY_WORKER_POOL = env.int('DISCOVERY_WORKER_POOL', default=10)
 IMPL = env.str('EXECUTOR_SERVICE_IMPL', 'PingExecutorService')
 
 # Telegram_bot
-
 BOT_TOKEN = env.str('BOT_TOKEN', default='')
 CHAT_ID = env.str('CHAT_ID', default='')
 
 # Gotify_server
-
 GOTIFY_IP = env.str('GOTIFY_IP', default='localhost')
 GOTIFY_PORT = env.int('GOTIFY_PORT', default=8080)
 GOTIFY_APP_TOKEN = env.str('GOTIFY_APP_TOKEN', default='')
