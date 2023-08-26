0dc08901223c6e923fcf5afca679b3586cb1f51f
Di11er <makarenko.stepan@bk.ru>
2023-08-27 00:59:04 +0300

    Изменил драйвер бд с SQLite на PostgreSQL

--- docker/web/models/controler.py
+++ docker/web/models/controler.py
@@ -1,6 +1,15 @@
 import config
 
-from peewee import SqliteDatabase
+from peewee import SqliteDatabase, PostgresqlDatabase
 
 
-db = SqliteDatabase(config.RESOURCES_DIR / 'db' / 'HealthMonitor.db')
+#db = SqliteDatabase(config.RESOURCES_DIR / 'db' / 'HealthMonitor.db')
+
+# Connect to a Postgres database.
+db = PostgresqlDatabase(
+    config.POSTGRES_DB, 
+    user=config.POSTGRES_USER, 
+    password=config.POSTGRES_PASSWORD,
+    host=config.POSTGRES_HOST, 
+    port=config.POSTGRES_PORT
+)
